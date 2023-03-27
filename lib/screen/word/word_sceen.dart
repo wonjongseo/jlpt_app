aec64dd03c1fbe3cc597f5aba06fa7ec631a50b6
kissco_wonjongseo <jswon@kissco.co.jp>
2023-03-27 12:04:37 +0900

    add condition color

--- lib/screen/word/word_sceen.dart
+++ lib/screen/word/word_sceen.dart
@@ -58,8 +58,13 @@ class _WordSceenState extends State<WordSceen> {
                   child: Stack(
                     alignment: AlignmentDirectional.center,
                     children: [
-                      SvgPicture.asset('assets/svg/calender.svg',
-                          color: AppColors.lightGreen),
+                      SvgPicture.asset(
+                        'assets/svg/calender.svg',
+                        color: controller.jlptSteps[step].scores ==
+                                controller.jlptSteps[step].words.length
+                            ? AppColors.lightGreen
+                            : null,
+                      ),
                       Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -75,6 +80,8 @@ class _WordSceenState extends State<WordSceen> {
                                     ?.copyWith(fontSize: (width / 10))),
                           ),
                           SizedBox(height: width / 100),
+                          // _progressbar(Size.fromWidth(width / 10),
+                          //     controller.jlptSteps[step].scores.toDouble()),
                           Center(
                             child: Text(
                               '${controller.jlptSteps[step].scores.toString()} / ${controller.jlptSteps[step].words.length}',
@@ -97,6 +104,38 @@ class _WordSceenState extends State<WordSceen> {
       }),
     );
   }
+}
+
+Color getColor(double range) {
+  if (range > 0 && range < 0.1) {
+    return Colors.redAccent;
+  } else if (range < 0.35) {
+    return Colors.yellowAccent;
+  } else if (range < 0.65) {
+    return Colors.orangeAccent;
+  } else {
+    return Colors.greenAccent;
+  }
+}
+
+Row _progressbar(Size size, double progressValue) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      SizedBox(
+        height: 10,
+        width: size.width / 1.5,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: LinearProgressIndicator(
+            backgroundColor: Colors.grey.withOpacity(0.2),
+            color: getColor(progressValue),
+            value: progressValue,
+          ),
+        ),
+      ),
+    ],
+  );
 }
 
 class StepCard extends StatelessWidget {
