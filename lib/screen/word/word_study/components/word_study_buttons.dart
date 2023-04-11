2ff989d361ac87f15d4b1b4323c7adf5d550187d
kissco_wonjongseo <jswon@kissco.co.jp>
2023-04-11 10:06:15 +0900

    animation

--- lib/screen/word/word_study/components/word_study_buttons.dart
+++ lib/screen/word/word_study/components/word_study_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:japanese_voca/screen/word/word_study/word_controller.dart';
@@ -11,43 +12,52 @@ class WordStudyButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    WordStudyController wordController = Get.find<WordStudyController>();
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomButton(
-              text: '의미',
-              onTap: () => wordController.showMean(),
-            ),
-            const SizedBox(width: 16),
-            CustomButton(
-              text: '읽는 법',
-              onTap: () => wordController.showYomikata(),
-            ),
-          ],
-        ),
-        const SizedBox(height: 16),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomButton(
-              text: '몰라요',
-              onTap: () {
-                wordController.nextWord(false);
-              },
-            ),
-            const SizedBox(width: 16),
-            CustomButton(
-              text: '알아요',
-              onTap: () {
-                wordController.nextWord(true);
-              },
-            ),
-          ],
-        ),
-      ],
-    );
+    return GetBuilder<WordStudyController>(builder: (wordController) {
+      return Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ZoomOut(
+                animate: wordController.isShownMean,
+                duration: const Duration(milliseconds: 300),
+                child: CustomButton(
+                  text: '의미',
+                  onTap: () => wordController.showMean(),
+                ),
+              ),
+              const SizedBox(width: 16),
+              ZoomOut(
+                animate: wordController.isShownYomikata,
+                duration: const Duration(milliseconds: 300),
+                child: CustomButton(
+                  text: '읽는 법',
+                  onTap: () => wordController.showYomikata(),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomButton(
+                text: '몰라요',
+                onTap: () {
+                  wordController.nextWord(false);
+                },
+              ),
+              const SizedBox(width: 16),
+              CustomButton(
+                text: '알아요',
+                onTap: () {
+                  wordController.nextWord(true);
+                },
+              ),
+            ],
+          ),
+        ],
+      );
+    });
   }
 }
